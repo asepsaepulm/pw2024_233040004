da4716221d29cd3b78b04e8c922fe3abb188b281
Asep Saepul Milah <146751939+asepsaepulmilah2003@users.noreply.github.com>
2024-03-10 19:36:35 +0700

    mengerjakan tugas

---
+++ kuliah/pertemuan5/latihan1.php
@@ -0,0 +1,50 @@
+<?php
+//  1. Membuat array
+$hari = array('Senin','Selasa','Rabu');
+$bulan = ['Januari','Februari','Maret'];
+$mahasiswa = ['Sandhika', 3.1, false];
+// 2. Mencetak isi array
+// Mencetak 1 nilai pada array, menggunakan index
+// index dimulai dari 0
+echo $hari[1];
+echo "<br>";
+echo $bulan[2];
+echo "<br>";
+echo $mahasiswa[1];
+echo "<hr>";
+
+// Cetak semua isi array
+// var_dump() atau print_r()
+// digunakan saat debugging
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+var_dump($mahasiswa);
+echo "<hr>";
+
+// 3. Memanipulasi isi array
+// Menambah isi array
+// di akhir : [] atau array_push()
+$hari[] = 'Kamis';
+$hari[] = 'Jumat';
+print_r($hari);
+echo "<br>";
+array_push($bulan, 'April', 'Mei');
+print_r($bulan);
+echo "<br>";
+
+// di awal: array_unshift()
+array_unshift($mahasiswa, '233040004');
+print_r($mahasiswa);
+echo "<hr>";
+
+// Menghapus isi array
+// di belakang: array_pop();
+// di depan: array_shift();
+array_pop($hari);
+array_shift($bulan);
+print_r($hari);
+echo "<br>";
+print_r($bulan);
+?>
---
+++ kuliah/pertemuan5/latihan2.php
@@ -0,0 +1,48 @@
+<?php
+$binatang = ["🐱", "🐰", "🙊", "🐻", "🐮", "🦊", "🐸"];
+$makanan = ["🍜", "🍝", "🍛", "🍲", "🍘", "🍣"]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Array</title>
+</head>
+<body>
+    <h3>Daftar Binatang</h3>
+    <ol>
+        <?php for ($i = 0; $i < count($binatang); $i++) { ?>
+        <li><?php echo $binatang[$i]; ?></li>
+        <?php } ?>
+    </ol>
+    <h3>Daftar Makanan</h3>
+    <ul>
+        <?php for ($i = 0; $i < count($makanan); $i++) { ?>
+        <li><?php echo $makanan[$i]; ?></li>
+        <?php } ?>
+        </ul>
+
+        <hr>
+
+        <h3>Daftar Binatang</h3>
+        <ol>
+            <?php foreach ($binatang as $b) { ?>
+            <li><?php echo $b; ?></li>
+            <?php } ?>
+        </ol>
+        <h3>Daftar Makanan</h3>
+        <ul>
+            <?php foreach ($makanan as $m) { ?>
+            <li><?php echo $m; ?></li>
+            <?php } ?>
+        </ul>
+        <h3>Daftar Makanan</h3>
+        <ul>
+            <?php foreach ($makanan as $m) : ?>
+            <li><?= $m; ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+</body>
+</html>
